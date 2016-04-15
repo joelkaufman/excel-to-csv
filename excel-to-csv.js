8fbd2c41ea88e7f27d745edf99716031e43e9ca5
relief <relief@mac-mini.home>
2016-04-15 14:34:10 -0400

    refactor

--- excel-to-csv.js
+++ excel-to-csv.js
@@ -1,31 +1,24 @@
+var stringify = require('csv-stringify');
 var xlsx = require('node-xlsx');
 var fs = require('fs');
-var stringify = require('csv-stringify');
 
-var src = process.argv[2];
-var dest = process.argv[3];
-
+var src = process.argv[2], dest = process.argv[3];
 
 var obj = xlsx.parse(src); // parses a file
 
 //looping through all sheets
 for(var i = 0; i < obj.length; i++)
 {
+    var sheet = obj[i], rows = [], writeStr = "";
 
-    var sheet = obj[i];
-    var rows = [];
-    var writeStr = "";
     //loop through all rows in the sheet
-
     for(var j = 0; j < sheet['data'].length; j++)
     {
         //add the row to the rows array
         rows.push(sheet['data'][j]);
     }
 
-
 	stringify(rows, saveTofile(sheet.name));
-
 
 	function saveTofile(sheetN){
 
